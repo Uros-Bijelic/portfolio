b0f2711b7a9983964a5a8c3351ae6291f0747d04
urke90 <urosbijelic90@gmail.com>
2024-07-08 19:17:52 +0200

    init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        white: {
+          100: '#FFFFFF',
+          300: '#DDE4FF',
+          800: '#CBCBFF',
+        },
+        blue: {
+          500: '#5EAFFF',
+        },
+        black: {
+          100: '#08080F',
+          500: '#1D1F2580',
+          600: '#1D1F25',
+          800: '#101014',
+        },
+        purple: {
+          500: '#CBACF9',
+        },
       },
     },
   },
