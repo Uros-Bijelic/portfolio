b9597aeeacac1f0fffed92e168db827c7cb21d78
urke90 <urosbijelic90@gmail.com>
2024-07-10 11:28:18 +0200

    add vortex animation from acertenity UI lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image';
-
-import NavBar from '@/components/NavBar';
+// import VortexHome from '@/components/VortexHome';
+import { Vortex } from '@/components/ui/Vortex';
 
 // ----------------------------------------------------------------
 
@@ -8,24 +7,28 @@ interface IHomeProps {}
 
 const Home: React.FC<IHomeProps> = () => {
   return (
-    <div className="flex h-full flex-col border">
-      <div className="mx-auto mt-12">
-        <NavBar />
-      </div>
-      <section className="h-full">
-        <Image
-          src="/assets/images/home-bg.svg"
-          alt="cover"
-          fill
-          // width="928"
-          // height="900"
-          className="object-cover"
-        />
-        <h1 className="mt-40">
-          Hi! I&apos;m Uroš , a Next.js Developer based in Serbia.
-        </h1>
-      </section>
+    // <div className="flex h-full flex-col border">
+    <div className="mx-auto h-screen  overflow-hidden rounded-md">
+      <Vortex
+        // backgroundColor="#101014"
+        rangeY={800}
+        particleCount={100}
+        // baseHue={120}
+        className="flex h-screen w-full flex-col items-center justify-center px-2 py-4 md:px-10"
+      >
+        <div className="max-w-screen-lg border">
+          <h2 className="text-center text-2xl font-bold text-white-100 md:text-6xl">
+            Hi! I’m Uroš, a Next.js Developer based in Serbia.
+          </h2>
+          <p className="p-medium text-white mt-6  text-center text-sm md:text-2xl ">
+            Web developer creating top-notch digital experiences with passion
+            and precision
+          </p>
+        </div>
+      </Vortex>
     </div>
+
+    // </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,35 @@
 import type { Config } from 'tailwindcss';
 
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
+
+// ----------------------------------------------------------------
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+};
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
+      screens: {
+        xxl: '1440px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -38,6 +60,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
 export default config;
