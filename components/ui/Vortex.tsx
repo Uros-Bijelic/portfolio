d2139c9ea275a9f5058c358bdbdd63b46f86029a
urke90 <urosbijelic90@gmail.com>
2024-08-06 11:24:08 +0200

    fix z-index

--- components/ui/Vortex.tsx
+++ components/ui/Vortex.tsx
@@ -243,12 +243,7 @@ export const Vortex: React.FC<IVortexProps> = (props) => {
   }, []);
 
   return (
-    <div
-      className={cn(
-        'relative w-full border z-[6000] h-screen',
-        props.containerClassName,
-      )}
-    >
+    <div className={cn('relative h-full w-full', props.containerClassName)}>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
